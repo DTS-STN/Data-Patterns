8cbd2488c5ba3dbdf3fc3e7b678003b70818f7ee
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2022-01-14 14:46:20 -0500

    Part of home page #87
    Adding part of the required update for the home page.
    
    Going to adjust styling to make content width global.

---
+++ content/en/home.md
@@ -0,0 +1,3 @@
+# Data Patterns Library
+
+Reusable patterns and components that give users more power over their data and help teams build trustworthy digital services. Learn more [about data patterns](https://dpl-bmd.alpha.service.canada.ca/about.html) and how you can use them.
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Home from '../components/templates/Home';
 
 export default function Index(props) {
     return (
-        <Home/>
+        <Home content={props.home}/>
     )
 }
 
@@ -13,9 +13,16 @@ export async function getStaticProps() {
     const markdownWithMetadata = fs.readFileSync("content/en/infoPanel.md").toString();
     const parsedMarkdown = matter(markdownWithMetadata);
 
+    const homeMarkdownMeta = fs.readFileSync("content/en/home.md").toString();
+    const parsedHomeMarkdown = matter(homeMarkdownMeta);
+
     return {
         props: {
             t: dictionary,
+            home: {
+                markdown: parsedHomeMarkdown.content,
+                data: parsedHomeMarkdown.data
+            },
             infoPanel: {
                 markdown: parsedMarkdown.content,
                 data: parsedMarkdown.data
