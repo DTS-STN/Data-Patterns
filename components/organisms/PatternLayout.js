fba46ced0618bd08e1ae1c4a93b8b2a0fc462a97
s-laugh <22419216+s-laugh@users.noreply.github.com>
2022-02-03 14:04:05 -0500

    remove conditions in required places

--- components/organisms/PatternLayout.js
+++ components/organisms/PatternLayout.js
@@ -7,17 +7,11 @@ export default function PatternLayout(props) {
 
     return (
         <MarkdownLayout markdown={props.pattern.markdown} pattern={props.pattern} t={props.t}>
-            {props.pattern.data.hasOwnProperty("status") &&
-                <Alert>
-                    <p>{props.t["This pattern is currently"]} <strong>{props.pattern.data.status}</strong></p>
-                </Alert>
-            }
-            {props.pattern.data.hasOwnProperty("feature-img-path") &&
-                <img className={styles.featuredImage} src={props.pattern.data["feature-img-path"]} alt={props.pattern.data["feature-img-alt"]}/>
-            }
-            {props.pattern.data.hasOwnProperty("short-description") &&
-                <p><strong>{props.pattern.data["short-description"]}</strong></p>   
-            }
+            <Alert>
+                <p>{props.t["This pattern is currently"]} <strong>{props.pattern.data.status}</strong></p>
+            </Alert>
+            <img className={styles.featuredImage} src={props.pattern.data["feature-img-path"]} alt={props.pattern.data["feature-img-alt"]}/>
+            <p><strong>{props.pattern.data["short-description"]}</strong></p>
         </MarkdownLayout>
     );
 }
